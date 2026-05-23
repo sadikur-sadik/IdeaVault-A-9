5f0ac872fbb257d8f057248e4969671c366017fd
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-05-23 14:35:37 +0600

    Idea Details: Each Idea's Comments Basic Setup

--- src/app/(ideas-sections)/ideas/[id]/page.jsx
+++ src/app/(ideas-sections)/ideas/[id]/page.jsx
@@ -1,18 +1,21 @@
 import Comment from "@/app/components/Comments/Comment";
+import CommentCard from "@/app/components/Comments/CommentCard";
 import IdeaCard from "@/app/components/Ideas/IdeasCard/IdeaCard";
 import { postComment } from "@/lib/action";
-import { getIdeasById } from "@/lib/data";
+import { getComments, getIdeasById } from "@/lib/data";
 
 const IdeaDetails = async({params}) => {
 
   const {id} = await params;
   const idea = await getIdeasById(id)
-  
+  const comments = await getComments()
+  const ideaComments = comments.filter(comment => comment.ideaID == id)
   
   return (
     <div>
       <IdeaCard idea={idea}></IdeaCard>
       <Comment postComment={postComment} id={id}></Comment>
+      {ideaComments.map(ideaComment => <CommentCard key={ideaComment?._id} comment={ideaComment}></CommentCard>)}
     </div>
   );
 };
