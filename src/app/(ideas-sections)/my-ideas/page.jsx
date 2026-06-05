c2ccad500c9bc652a21674851523b9b95d00e370
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-06-05 23:41:19 +0600

    Responsiveness issue

--- src/app/(ideas-sections)/my-ideas/page.jsx
+++ src/app/(ideas-sections)/my-ideas/page.jsx
@@ -27,7 +27,7 @@ const myIdeas = async () => {
        ?
         <NoIdeas/>
         :
-        <div className="grid md:grid-cols-3  grid-cols-1 gap-6">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3  grid-cols-1 md:gap-6 gap-3">
           {userIdeas.map(idea => <MyIdeaCard idea={idea} key={idea?._id} deleteIdea={deleteIdea} updateIdea={updateIdea}/>)}
         </div>}
       </div>
